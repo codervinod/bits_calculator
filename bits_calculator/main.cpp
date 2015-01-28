ef7cc25db584056cd1d47989245d4af434d79088
Vinod Gupta <vinod_gupta@live.com>
2015-01-28 15:06:56 -0800

    initial check in

--- bits_calculator/main.cpp
+++ bits_calculator/main.cpp
@@ -8,8 +8,46 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int brootForceNumSetBits(unsigned long long num)
+{
+    int numSetBits = 0;
+    for (int i = 0; i < (sizeof(num)*8); ++i)
+        numSetBits += ((num >> i) & 0x1);
+    
+    return numSetBits;
+}
+
+int optiHash1[256];
+
+void initOptimal1()
+{
+    for (int i = 0; i < sizeof optiHash1 / sizeof(int); ++i)
+    {
+        optiHash1[i] = brootForceNumSetBits(i);
+    }
+}
+
+int optimal1NumSetBits(unsigned long long num)
+{
+    int numSetBits = 0;
+    for (int i = 0; i < sizeof(num); ++i)
+    {
+        numSetBits += optiHash1[(num >> (i * 8)) & 0xFF];
+    }
+    return numSetBits;
+}
+
+int main(int argc, char* argv[])
+{
+    unsigned long long num = 0xFFFFFFFFFFFFFFFF;
+    std::cout << "Number of bits set in " <<
+    num << " = " <<
+    brootForceNumSetBits(num)<<std::endl;
+    std::cout << "Opti1 trial";
+    initOptimal1();
+    
+    std::cout << "Number of bits set in " <<
+    num << " = " <<
+    optimal1NumSetBits(num) << std::endl;
     return 0;
 }
